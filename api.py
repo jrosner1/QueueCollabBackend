f13d9070206cf50e5d1cc041d83bcb03f1c7acbe
Jason Rosner <38009477+jrosner1@users.noreply.github.com>
2021-05-18 13:11:18 -0400

    Set up firebase and started with user authentication via JWT. Need to work on models next.

--- api.py
+++ api.py
@@ -1,17 +1,28 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from firebase_admin import credentials, initialize_app, db
+import views, models, resources
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return {"data": name}
+# Initialize Firestore DB
+cred_obj = credentials.Certificate('cred.json')
+default_app = initialize_app(cred_obj, {
+	'databaseURL':'https://spotifyparty-a6660-default-rtdb.firebaseio.com/'
+	})
+db = db.reference("/")
 
-    def post(self):
-        return {'data': "posted"}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
 
 if __name__ == '__main__':
     app.run(debug=True)
